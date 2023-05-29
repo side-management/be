a1a273e39b901901894efb78e8a1e013cb9c6dc1
YoungKyonYou <man.spride1324@gmail.com>
2023-05-29 14:12:57 +0900

    initial commit

---
+++ src/test/java/com/example/sidemanagementbe/SideManagementBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sidemanagementbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SideManagementBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
