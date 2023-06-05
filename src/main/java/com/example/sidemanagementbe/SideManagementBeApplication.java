b79d80c05acb463b3682ff19521d22a50ee79762
YoungKyonYou <man.spride1324@gmail.com>
2023-06-05 20:54:19 +0900

    feat: 카카오 소셜 로그인 추가

--- src/main/java/com/example/sidemanagementbe/SideManagementBeApplication.java
+++ src/main/java/com/example/sidemanagementbe/SideManagementBeApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SideManagementBeApplication {
-    /*
-     * test init commit
-     * */
+
     public static void main(String[] args) {
         SpringApplication.run(SideManagementBeApplication.class, args);
     }
