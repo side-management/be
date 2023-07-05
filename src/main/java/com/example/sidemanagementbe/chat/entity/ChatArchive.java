c42c9ced54acbf0e37bee2d66f63ca957541b40f
YoungKyonYou <man.spride1324@gmail.com>
2023-07-05 22:31:16 +0900

    feat: ChatArchive 자바 파일 추가

---
+++ src/main/java/com/example/sidemanagementbe/chat/entity/ChatArchive.java
@@ -0,0 +1,43 @@
+package com.example.sidemanagementbe.chat.entity;
+
+import com.example.sidemanagementbe.chat.dto.SystemMessageType;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@Table(name = "saida_archive_chat")
+public class ChatArchive {
+
+    //team_id와 같음
+    @Id
+    @GeneratedValue
+    Long id;
+
+    @Column(nullable = false)
+    private Long memberId;
+
+    private String senderNickname;
+
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    private SystemMessageType messageType;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
