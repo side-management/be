9de0399c8adf422cf75e24c7343de12a45c25573
JaehongDev <println@kakao.com>
2023-07-09 01:26:06 +0900

    feat: 로그인 api 명세 구현

--- src/main/java/com/example/sidemanagementbe/auth/controller/AuthController.java
+++ src/main/java/com/example/sidemanagementbe/auth/controller/AuthController.java
@@ -1,13 +1,15 @@
 package com.example.sidemanagementbe.auth.controller;
 
 
+import com.example.sidemanagementbe.auth.payload.request.SignInRequest;
 import com.example.sidemanagementbe.auth.payload.request.SignUpRequest;
+import com.example.sidemanagementbe.auth.payload.response.SignInResponse;
 import com.example.sidemanagementbe.auth.payload.response.SignUpResponse;
+import com.example.sidemanagementbe.auth.service.impl.SignInService;
 import com.example.sidemanagementbe.auth.service.impl.SignUpService;
 import javax.annotation.security.PermitAll;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,10 +22,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
     private final SignUpService signUpService;
+    private final SignInService signInService;
 
     @PermitAll
     @PostMapping("/signup")
-    public ResponseEntity<SignUpResponse> signup(@Validated @RequestBody SignUpRequest signUpRequest) {
-        return ResponseEntity.ok(signUpService.execute(signUpRequest));
+    public SignUpResponse signup(@Validated @RequestBody SignUpRequest signUpRequest) {
+        return signUpService.execute(signUpRequest);
+    }
+
+    @PermitAll
+    @PostMapping("/signin")
+    public SignInResponse signin(@Validated @RequestBody SignInRequest signInRequest) {
+        return signInService.execute(signInRequest);
     }
 }
