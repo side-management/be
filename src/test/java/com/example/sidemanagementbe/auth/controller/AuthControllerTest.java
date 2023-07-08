4340c1228b6dcb77a0c731c2fb1fe18cc3633bf6
JaehongDev <println@kakao.com>
2023-07-09 01:26:26 +0900

    test: 로그인 api 단위 테스트 구현

--- src/test/java/com/example/sidemanagementbe/auth/controller/AuthControllerTest.java
+++ src/test/java/com/example/sidemanagementbe/auth/controller/AuthControllerTest.java
@@ -2,13 +2,17 @@ package com.example.sidemanagementbe.auth.controller;
 
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.example.sidemanagementbe.auth.payload.request.SignInRequest;
 import com.example.sidemanagementbe.auth.payload.request.SignUpRequest;
+import com.example.sidemanagementbe.auth.payload.response.SignInResponse;
 import com.example.sidemanagementbe.auth.payload.response.SignUpResponse;
+import com.example.sidemanagementbe.auth.service.impl.SignInService;
 import com.example.sidemanagementbe.auth.service.impl.SignUpService;
 import com.example.sidemanagementbe.web.security.config.SecurityConfig;
 import com.example.sidemanagementbe.web.security.util.JwtTokenProvider;
@@ -18,7 +22,6 @@ import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -26,6 +29,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @WebAppConfiguration
@@ -40,48 +44,89 @@ class AuthControllerTest {
     private ObjectMapper objectMapper;
     @MockBean
     private SignUpService signUpService;
+    @MockBean
+    private SignInService signInService;
 
 
     @Nested
     @DisplayName("회원 가입 api 단위 테스트에서")
     class SignUpApiTest {
-
         @Test
         void 토큰은_필수적으로_필요합니다() throws Exception {
             var signupRequest = SignUpRequest.builder()
                     .accessToken(null)
                     .build();
-            mockMvc.perform(post("/auth/signup")
+            getSignUpResultActions(signupRequest)
+                    .andExpect(status().isBadRequest());
+        }
+
+
+        @Test
+        void 성공적으로_응답을_내려준다() throws Exception {
+            var signUpResponse = SignUpResponse.builder()
+                    .accessToken("accessToken")
+                    .refreshToken("refreshToken")
+                    .build();
+            var signUpRequest = SignUpRequest.builder()
+                    .accessToken("아무거나")
+                    .build();
+            given(signUpService.execute(any())).willReturn(signUpResponse);
+
+            getSignUpResultActions(signUpRequest)
+                    .andExpectAll(
+                            status().isOk(),
+                            jsonPath("$.accessToken").value(signUpResponse.getAccessToken()),
+                            jsonPath("$.refreshToken").value(signUpResponse.getRefreshToken())
+                    );
+        }
+
+        private ResultActions getSignUpResultActions(SignUpRequest signupRequest) throws Exception {
+            return mockMvc.perform(post("/auth/signup")
                             .contentType(MediaType.APPLICATION_JSON)
                             .content(objectMapper.writeValueAsBytes(signupRequest)))
-                    .andDo(print())
-                    .andExpect(status().isBadRequest());
+                    .andDo(print());
+        }
+    }
+
+
+    @Nested
+    @DisplayName("로그인 api 단위 테스트에서")
+    class SignInApiTest {
+        @Test
+        void 토큰은_필수적으로_필요합니다() throws Exception {
+            var signInRequest = SignInRequest.builder()
+                    .accessToken(null)
+                    .build();
+            getSignInResultActions(signInRequest)
+                    .andExpectAll(status().isBadRequest());
         }
 
         @Test
         void 성공적으로_응답을_내려준다() throws Exception {
-            BDDMockito.given(signUpService.execute(any())).willReturn(SignUpResponse.builder()
+            var signInResponse = SignInResponse.builder()
                     .accessToken("accessToken")
-                    .refreshToken("refreshToken").build());
-            var signUpRequest = SignUpRequest.builder()
+                    .refreshToken("refreshToken")
+                    .build();
+            var signInRequest = SignInRequest.builder()
                     .accessToken("아무거나")
                     .build();
+            given(signInService.execute(any())).willReturn(signInResponse);
 
-            mockMvc.perform(post("/auth/signup")
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content(objectMapper.writeValueAsBytes(signUpRequest)))
-                    .andDo(print())
+            getSignInResultActions(signInRequest)
                     .andExpectAll(
                             status().isOk(),
-                            jsonPath("$.accessToken").exists(),
-                            jsonPath("$.refreshToken").exists(),
-                            jsonPath("$.accessToken").value("accessToken"),
-                            jsonPath("$.refreshToken").value("refreshToken")
+                            jsonPath("$.accessToken").value(signInResponse.getAccessToken()),
+                            jsonPath("$.refreshToken").value(signInResponse.getRefreshToken())
                     );
-
-
         }
 
+        private ResultActions getSignInResultActions(SignInRequest signInRequest) throws Exception {
+            return mockMvc.perform(post("/auth/signin")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsBytes(signInRequest)))
+                    .andDo(print());
+        }
     }
+
 
 }
