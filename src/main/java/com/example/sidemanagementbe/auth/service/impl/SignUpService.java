ea542f2b6161df372b70939a6d2905e011b2850b
JaehongDev <println@kakao.com>
2023-07-09 01:04:39 +0900

    feat: 회원가입 페이로드(request,response) 정의

---
+++ src/main/java/com/example/sidemanagementbe/auth/service/impl/SignUpService.java
@@ -0,0 +1,13 @@
+package com.example.sidemanagementbe.auth.service.impl;
+
+
+import com.example.sidemanagementbe.auth.payload.request.SignUpRequest;
+import com.example.sidemanagementbe.auth.payload.response.SignUpResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SignUpService {
+    public SignUpResponse execute(SignUpRequest signUpRequest) {
+        return null;
+    }
+}
