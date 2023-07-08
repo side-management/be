18cbbdb33b6f1517a4f35f80f681021c8875e8fa
JaehongDev <println@kakao.com>
2023-07-09 01:05:35 +0900

    refactor: 어플리케이션 진입점에 존재하는 설정을 별도의 클래스로 분리

---
+++ src/main/java/com/example/sidemanagementbe/web/SchedulingConfiguration.java
@@ -0,0 +1,11 @@
+package com.example.sidemanagementbe.web;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+@EnableScheduling
+@Configuration
+public class SchedulingConfiguration {
+}
