7ccf10905d3d0fbc11bba59e6126450403d89f7d
JaehongDev <println@kakao.com>
2023-07-09 01:07:20 +0900

    feat: 회원가입 응답 페이로드 정의

---
+++ src/main/java/com/example/sidemanagementbe/auth/payload/request/SignUpRequest.java
@@ -0,0 +1,18 @@
+package com.example.sidemanagementbe.auth.payload.request;
+
+
+import javax.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class SignUpRequest {
+    @NotNull
+    private String accessToken;
+}
